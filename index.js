d63e1f7de0cf307e79db94e5d67f4fe7299b95c0
Frank Bishop <ffbishop3@gmail.com>
2021-07-03 16:33:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 exports.handler = async (event, context, callback) => {
-  // TODO implement
-  // const response = {
-  //     statusCode: 200,
-  //     body: JSON.stringify('Hello from Lambda!'),
-  // };
+  //declare array of vanity numbers
+  //pull number from event and set it to a variable
+  //covert number to string
+  //set max number to 25
+  //have a while loop that increases max at the end and runs until max is 25
+  //loop through string and have a switch statement that based on what number it is, it randomly picks one letter for each number
+  //loop returns vanity number
+  //outer while loop pushes vanity number into an array of vanity numbers
+  //function returns vanity numbers array
   let test2 = 123;
   test2 = test2.toString();
   function vanity(number) {
